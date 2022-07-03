854fa09fc18c078629bba23432bc70245f219150
hb <82237720+hbbots@users.noreply.github.com>
2022-07-03 12:39:36 +0530

    Create HB.py

---
+++ HB.py
@@ -0,0 +1,195 @@
+import os
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+from pyrogram import Client, filters,emoji
+from pyrogram.types import Message
+import pyqrcode
+import png
+
+HB = Client(
+    "YOUTUBE Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)  
+
+START_TEXT = """**
+HI {}, 
+I AM A QR CODE GENERATOR BOT
+MADE BY @TELSABOTS**"""
+
+HELP_TEXT = """**
+SENT ANY URL .......
+THEN CHOOSE ANY OPTION...
+THEN WAIT....
+MADE BY @TELSABOTS**
+"""
+
+ABOUT_TEXT = """
+ 🤖<b>BOT :QR CODE GENERATOR </b>
+ 
+ 🧑🏼‍💻DEV🧑🏼‍💻: @ALLUADDICT
+ 
+ 📢<b>CHANNEL :</b>@TELSABOTS
+ 
+ 📝<b>Language :</b>  <a href='https://python.org/'>Python3</a>
+ 
+ 🧰<b>Frame Work :</b>  <a href='https://pyrogram.org/'>Pyrogram</a>
+ 
+ 🤩<b>SOURCE :</b>  <a href='http://www.youtube.com/watch?v=nfWjbuQqgJc'>CLICK HERE</a>
+ 
+ 
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
+        InlineKeyboardButton('🆘HELP🆘', callback_data='help'),
+        InlineKeyboardButton('🤗ABOUT🤗', callback_data='about'),
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+
+
+result_buttons = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
+        InlineKeyboardButton('🏡HOME🏡', callback_data='home'),
+        InlineKeyboardButton('🤗ABOUT🤗', callback_data='about'),
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
+        InlineKeyboardButton('🏡HOME🏡', callback_data='home'),
+        InlineKeyboardButton('🆘HELP🆘', callback_data='help'),
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+
+SOURCE_TEXT = """</b>PRESS SOURCE BUTTON \n FOR SOURCE CODE</b>"""
+SOURCE_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('✅SOURCE✅', url='http://www.youtube.com/watch?v=nfWjbuQqgJc'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+
+result_buttons = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+
+result_buttons2 = InlineKeyboardMarkup(
+    [[
+        InlineKeyboardButton('✅GENERATE✅ ' , callback_data='generate')
+    ],[
+        InlineKeyboardButton('❌CANCEL❌', callback_data='close')
+    ]]
+   )
+result_text = """**JOIN @TELSABOTS**"""
+
+
+@HB.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+@HB.on_message(filters.command(["help"]))
+async def help_message(bot, update):
+    text = HELP_TEXT
+    reply_markup = HELP_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+@HB.on_message(filters.command(["about"]))
+async def about_message(bot, update):
+    text = ABOUT_TEXT
+    reply_markup = ABOUT_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+@HB.on_message(filters.command(["source"]))
+async def about_message(bot, update):
+    text = SOURCE_TEXT
+    reply_markup = SOURCE_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+@HB.on_message()
+async def main(client:Client, message:Message):
+    global url
+    global var
+    var = message.text
+    url = message.text
+    photo=pyqrcode.create(url)
+    photo.png('HB.png',scale=20)
+    await message.reply_text(
+        text="**CHOOSE ANY OPTION 👇🏻**",
+        reply_markup=result_buttons2,
+        quote=True,
+    )
+
+@HB.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    elif update.data == "generate":
+        await  HB.send_photo(
+        chat_id = update.message.chat.id,
+        photo='hb.png',
+        caption=result_text,
+        reply_markup=result_buttons
+        )  
+        await update.message.delete()
+    else:
+        await update.message.delete()
+print("HB")
+HB.run()
